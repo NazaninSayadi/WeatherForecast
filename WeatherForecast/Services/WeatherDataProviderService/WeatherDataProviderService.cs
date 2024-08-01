83bc1bbbc9681318d06fa23d1520a17f51322b93
n.sayadi <nazanin.sayadi@live.com>
2024-08-01 14:33:18 +0330

    use data repositories in WeatherDataProviderService

--- WeatherForecast/Services/WeatherDataProviderService/WeatherDataProviderService.cs
+++ WeatherForecast/Services/WeatherDataProviderService/WeatherDataProviderService.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Caching.Memory;
+using WeatherForecast.Data;
 using WeatherForecast.Services.ExternalWeatherService;
 
 namespace WeatherForecast.Services.WeatherDataProviderService;
@@ -5,24 +7,48 @@ namespace WeatherForecast.Services.WeatherDataProviderService;
 public class WeatherDataProviderService : IWeatherDataProviderService
 {
     private readonly IWeatherService _weatherService;
+    private readonly IWeatherDataCommandRepository _weatherDataCommandRepository;
+    private readonly IWeatherDataQueryRepository _weatherDataQueryRepository;
+    private readonly IMemoryCache _cache;
 
-    public WeatherDataProviderService(IWeatherService weatherService)
+    public WeatherDataProviderService(IWeatherService weatherService,
+                                      IWeatherDataCommandRepository weatherDataCommandRepository,
+                                      IWeatherDataQueryRepository weatherDataQueryRepository,
+                                      IMemoryCache cache)
     {
         _weatherService = weatherService;
+        _weatherDataCommandRepository = weatherDataCommandRepository;
+        _weatherDataQueryRepository = weatherDataQueryRepository;
+        _cache = cache;
     }
 
     public async Task<string?> Fetch()
     {
+        const string cacheKey = "WeatherData";
         try
         {
             var weatherData = await _weatherService.FetchWeatherDataAsync();
 
-            //Save to db            
+            _ =  Task.Run(() =>
+            {
+                _weatherDataCommandRepository.AddOrUpdateAsync(weatherData);
+                _cache.Set(cacheKey, weatherData, TimeSpan.FromMinutes(10));
+            });
+
             return weatherData;
         }
         catch (Exception ex)
         {
-            //read from db
+            if (_cache.TryGetValue(cacheKey, out string? cachedData))
+            {
+                return cachedData;
+            }
+
+            if (await _weatherDataQueryRepository.GetAsync() is { } dbData)
+            {
+                return dbData;
+            }
+
             return null;
         }
     }
